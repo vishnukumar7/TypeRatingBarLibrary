cd0036f5a4c08431c99639cc1a2e1167ff0888e3
vishnukumar.gr <vishnukumar.gr@changepond.com>
2023-12-19 17:00:12 +0530

    init commit

--- app/src/main/java/com/github/ratingbarlibrary/MainActivity.kt
+++ app/src/main/java/com/github/ratingbarlibrary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.ratingbarlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.samplerating.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
